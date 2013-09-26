4766822e3ad595b79b22c40ca23f02302f5384e5
clblamires <superstudent@mlh240-07.lcsc.edu>
2013-09-26 12:07:19 -0700

    Initial Commit

---
+++ RSS-Reader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RSS-Reader
+//
+//  Created by Super Student on 9/26/13.
+//  Copyright (c) 2013 CS-360_Students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
