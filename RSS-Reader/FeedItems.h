07591fec82b458dc4a144f34bd36db1b571954b6
Ryan Spiekerman <rsspiekerman@lcmail.lcsc.edu>
2013-09-30 11:41:35 -0700

    Filled FeedItems.h and .m with block comment code.

--- RSS-Reader/FeedItems.h
+++ RSS-Reader/FeedItems.h
@@ -19,3 +19,12 @@
 @end
 
 
+/*
+ #import <UIKit/UIKit.h>
+ 
+ @interface APPMasterViewController : UITableViewController <NSXMLParserDelegate>
+ 
+ @property (strong, nonatomic) IBOutlet UITableView *tableView;
+ 
+ @end
+*/
